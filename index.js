246a6bb3d0bf412fdb13f980b894e5fe4eb543cb
Dennis <denkipyator@gmail.com>
2023-12-12 15:26:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function distanceFromHqInBlocks(destination){
+    let result = (destination - 42)
+    return(Math.abs(result))
+};
+function distanceFromHqInFeet(feet){
+    return(distanceFromHqInBlocks(feet) * 264)
+};
+function distanceTravelledInFeet(pointA,pointB){
+    let disCovered = (pointA - pointB)
+    return(Math.abs(disCovered) * 264)
+};
+function calculatesFarePrice(pointX,pointY){
+    let disInBlocks = Math.abs(pointX - pointY);
+    let disTravelled = disInBlocks * 264;
+    if(disTravelled <= 400){
+        return(0)
+    }
+    else if(disTravelled > 400 && disTravelled < 2000){
+        let calculableDistance = disTravelled - 400;
+        return(calculableDistance * .02)
+    }
+    else if(disTravelled > 2000 && disTravelled < 2500){
+        return(25)
+    }
+    else{
+        return('cannot travel that far')
+    }
+};
+
+
 // Code your solution in this file!
